5307aa3057eb81b49456b8e4d5bbbca646538407
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2019-10-28 20:10:41 +0100

    Update game app component

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
+import { createStage } from '../gameHelpers'
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 import { usePlayer } from '../hooks/usePlayer'
 import { useStage } from '../hooks/useStage'
@@ -9,12 +10,46 @@ import { useStage } from '../hooks/useStage'
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
-
-    const [player] = usePlayer()
+    const [player, updatePlayerPos, resetPlayer] = usePlayer();
     const [stage, setStage] = useStage(player)
 
+    const movePlayer = dir => {
+        updatePlayerPos({ x: dir, y: 0 })
+    }
+
+    const startGame = () => {
+        setStage(createStage())
+        resetPlayer()
+    }
+
+    const drop = () => {
+        updatePlayerPos({ x: 0, y: 1, collided: false })
+    }
+
+    const dropPlayer = () => {
+        drop()
+    }
+
+    const move = ({ keyCode }) => {
+        if (!gameOver) {
+            if (keyCode === 37) {
+                movePlayer(-1)
+            }
+            else if (keyCode === 39) {
+                movePlayer(1)
+            }
+            else if (keyCode === 40) {
+                dropPlayer()
+            }
+        }
+    }
+
     return (
-        <StyledTetrisWrapper>
+        <StyledTetrisWrapper
+            role='button'
+            tabIndex='0'
+            onKeyDown={e => move(e)}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
@@ -28,7 +63,7 @@ const Tetris = () => {
                             </div>
                         )}
 
-                    <StartButton />
+                    <StartButton callBack={startGame()} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
