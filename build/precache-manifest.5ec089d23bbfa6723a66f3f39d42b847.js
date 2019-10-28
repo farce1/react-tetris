be7af7850c92b7590121bb7a5a5d3db855e8a0f8
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2019-10-28 18:54:47 +0100

    gh-pages deploy files

---
+++ build/precache-manifest.5ec089d23bbfa6723a66f3f39d42b847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53282fa77c0b4eb2a9ced7b1a7625f8b",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "3ac12287a54f904530f8",
+    "url": "/react-tetris/static/css/main.02c38205.chunk.css"
+  },
+  {
+    "revision": "9e24362e67aa98e78e68",
+    "url": "/react-tetris/static/js/2.9d48bd49.chunk.js"
+  },
+  {
+    "revision": "3ac12287a54f904530f8",
+    "url": "/react-tetris/static/js/main.c0c9cf72.chunk.js"
+  },
+  {
+    "revision": "82e181c5811481a5a2c5",
+    "url": "/react-tetris/static/js/runtime-main.ad7ea4ba.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
