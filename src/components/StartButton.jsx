bd4cddf23eaf6caf7b439b801b82ec4a640ccb4d
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2019-10-29 23:21:29 +0100

    Make upper arrow rotating the tetromino

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 import { StyledStartButton } from './styles/StyledStartButton'
 
-const StartButton = ({ callBack }) => (
-    <StyledStartButton
-        onClick={callBack}
-    >
-        start game
+const StartButton = ({ onClick }) => (
+    <StyledStartButton onClick={onClick}>
+        START GAME
     </StyledStartButton>
 )
 
--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
-import { createStage } from '../gameHelpers'
+import { createStage, checkCollision } from '../gameHelpers'
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 import { usePlayer } from '../hooks/usePlayer'
 import { useStage } from '../hooks/useStage'
@@ -10,37 +10,52 @@ import { useStage } from '../hooks/useStage'
 const Tetris = () => {
     const [dropTime, setDropTime] = useState(null)
     const [gameOver, setGameOver] = useState(false)
-    const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player)
+    const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+    const [stage, setStage] = useStage(player, resetPlayer)
 
     const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0 })
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 })
+        }
     }
 
     const startGame = () => {
         setStage(createStage())
         resetPlayer()
+        setGameOver(false)
     }
 
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false })
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false })
+        } else {
+            if (player.pos.y < 1) {
+                setGameOver(true)
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true })
+        }
     }
 
     const dropPlayer = () => {
         drop()
+
     }
 
     const move = ({ keyCode }) => {
-        if (!gameOver) {
-            if (keyCode === 37) {
+        switch (!gameOver) {
+            case keyCode === 37:
                 movePlayer(-1)
-            }
-            else if (keyCode === 39) {
+                break
+            case keyCode === 39:
                 movePlayer(1)
-            }
-            else if (keyCode === 40) {
+                break
+            case keyCode === 40:
                 dropPlayer()
-            }
+                break
+            case keyCode === 38:
+                playerRotate(stage, 1)
+                break
         }
     }
 
@@ -63,7 +78,7 @@ const Tetris = () => {
                             </div>
                         )}
 
-                    <StartButton callBack={startGame()} />
+                    <StartButton onClick={startGame} />
                 </aside>
             </StyledTetris>
         </StyledTetrisWrapper>
